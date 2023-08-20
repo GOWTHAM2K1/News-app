58a09e6192cffe7b410518a0b74f3f32f54f811e
GOWTHAM2K1 <97494803+GOWTHAM2K1@users.noreply.github.com>
2023-08-21 00:50:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,34 +6,51 @@ import {
   Route,
   Routes
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize = 9;
-  newsApi = process.env.apiName //get a api from news api and store it in env file so that it doesnt get misused
+  state ={
+    progress:0
+  }
 
+
+  setProgress=(progress)=>{
+    this.setState({
+      progress:progress
+
+    })
+  }
+  
   render() {
+    // this.setProgress(80) can set progress by calling the function
     return (
       <div>
         <Router>
           <NavBar />
-         {/* <News pageSize={this.pageSize} country={'in'} category={'general'} />  */}
+          <LoadingBar
+          color='#f11946'
+          progress={this.state.progress}
+          
+        />
+         {/* <News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'general'} />  */}
           <Routes>
 
-            <Route exact path="/general" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'general'} key='general' />}></Route>
+            <Route exact path="/general" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'general'} key='general' />}></Route>
 
-            <Route exact path="/business" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'business'} key='business' />}></Route>
+            <Route exact path="/business" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'business'} key='business' />}></Route>
 
-            <Route exact path="/entertainment" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'entertainment'} key='entertainment' />}></Route>
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'entertainment'} key='entertainment' />}></Route>
 
-            <Route exact path="/general" element={<News pageSize={this.pageSize} country={'in'} newsApi={this.newsApi} category={'general'} key='general' />}></Route>
+            <Route exact path="/general" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'general'} key='general' />}></Route>
 
-            <Route exact path="/health" element={<News pageSize={this.pageSize} country={'in'} newsApi={this.newsApi} category={'health'} key='health' />}></Route>
+            <Route exact path="/health" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'health'} key='health' />}></Route>
 
-            <Route exact path="/science" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'science'} key='science' />}></Route>
+            <Route exact path="/science" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'science'} key='science' />}></Route>
 
-            <Route exact path="/sports" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'sports'} key='sports' />}></Route>
+            <Route exact path="/sports" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'sports'} key='sports' />}></Route>
 
-            <Route exact path="/technology" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'technology'} key='technology' />}></Route>
+            <Route exact path="/technology" element={<News setProgress={this.setProgress}rogress={this.setProgress} pageSize={this.pageSize} country={'in'} category={'technology'} key='technology' />}></Route>
 
 
           </Routes>
