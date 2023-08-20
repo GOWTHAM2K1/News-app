0794b1cbdeab40a14e9ccec90d992698bf52bf10
GOWTHAM2K1 <97494803+GOWTHAM2K1@users.noreply.github.com>
2023-08-20 15:55:52 +0530

    Update App.js
    news api stored in local

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 
 export default class App extends Component {
   pageSize = 9;
+  newsApi = process.env.apiName //get a api from news api and store it in env file so that it doesnt get misused
 
   render() {
     return (
@@ -18,21 +19,21 @@ export default class App extends Component {
          {/* <News pageSize={this.pageSize} country={'in'} category={'general'} />  */}
           <Routes>
 
-            <Route exact path="/general" element={<News pageSize={this.pageSize} country={'in'} category={'general'} key='general' />}></Route>
+            <Route exact path="/general" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'general'} key='general' />}></Route>
 
-            <Route exact path="/business" element={<News pageSize={this.pageSize} country={'in'} category={'business'} key='business' />}></Route>
+            <Route exact path="/business" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'business'} key='business' />}></Route>
 
-            <Route exact path="/entertainment" element={<News pageSize={this.pageSize} country={'in'} category={'entertainment'} key='entertainment' />}></Route>
+            <Route exact path="/entertainment" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'entertainment'} key='entertainment' />}></Route>
 
-            <Route exact path="/general" element={<News pageSize={this.pageSize} country={'in'} category={'general'} key='general' />}></Route>
+            <Route exact path="/general" element={<News pageSize={this.pageSize} country={'in'} newsApi={this.newsApi} category={'general'} key='general' />}></Route>
 
-            <Route exact path="/health" element={<News pageSize={this.pageSize} country={'in'} category={'health'} key='health' />}></Route>
+            <Route exact path="/health" element={<News pageSize={this.pageSize} country={'in'} newsApi={this.newsApi} category={'health'} key='health' />}></Route>
 
-            <Route exact path="/science" element={<News pageSize={this.pageSize} country={'in'} category={'science'} key='science' />}></Route>
+            <Route exact path="/science" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'science'} key='science' />}></Route>
 
-            <Route exact path="/sports" element={<News pageSize={this.pageSize} country={'in'} category={'sports'} key='sports' />}></Route>
+            <Route exact path="/sports" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'sports'} key='sports' />}></Route>
 
-            <Route exact path="/technology" element={<News pageSize={this.pageSize} country={'in'} category={'technology'} key='technology' />}></Route>
+            <Route exact path="/technology" element={<News pageSize={this.pageSize} newsApi={this.newsApi} country={'in'} category={'technology'} key='technology' />}></Route>
 
 
           </Routes>
